b154e047a2554980577b26fffd51c0c8734ae90b
Mahmoudi Mohamed Amine <mohamed.mahmoudi@ensia.edu.dz>
2023-08-05 20:41:05 +0100

    Drop_Down_Menu

--- app/views/Header.php
+++ app/views/Header.php
@@ -5,11 +5,41 @@
 
     <nav>
         <ul class="NavBarList">
-            <li><a href="#">الرئيسية</a></li>
-            <li><a href="#">خدمات</a></li>
-            <li><a href="#">مشاريع</a></li>
-            <li><a href="#">جديد</a></li>
-            <li><a href="#">إتصل بنا</a></li>
+            <li><a href="#">الرئيسية</a>
+                <ul class="DropDownMenu">
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                </ul>
+            </li>
+            <li><a href="#">خدمات</a>
+                <ul class="DropDownMenu">
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                </ul>
+            </li>
+            <li><a href="#">مشاريع</a>
+                <ul class="DropDownMenu">
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                </ul>
+            </li>
+            <li><a href="#">جديد</a>
+                <ul class="DropDownMenu">
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                </ul>
+            </li>
+            <li><a href="#">إتصل بنا</a>
+                <ul class="DropDownMenu">
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                    <li><a href="#">الرئيسية</a></li>
+                </ul>
+            </li>
         </ul>
     </nav>
 
