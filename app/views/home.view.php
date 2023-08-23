7aa82597b4f4b7156ff03f4313687065a3de21b4
Mahmoudi Mohamed Amine <mohamed.mahmoudi@ensia.edu.dz>
2023-08-24 00:26:35 +0100

    Some_Cleaning_And_Fixes

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= My_Files_Root ?>assets/CSS/swiper-bundle.min.css" />
     <title>Foodz</title>
 </head>
 
@@ -20,7 +19,7 @@
         <section class="Cover">
             <div class="Slides_Container_Div">
                 <div class="Slider">
-                    <img class="Slides_Previous_Btn" id="Slides_Previous_Btn" src="<?= My_Files_Root ?>assets/images/arrow-left-over.svg" alt="Previous">
+                    <img class="Slides_Previous_Btn" id="Slides_Previous_Btn" src="<?= My_Files_Root ?>assets/images/arrow-left-out.svg" alt="Previous">
                     <div class="Slides_Container">
                         <div class="Slide"><img src="<?= My_Files_Root ?>assets/images/slider-main/bg1.jpg" alt="background" height="600px" width="100%"></div>
                         <div class="Slide"><img src="<?= My_Files_Root ?>assets/images/slider-main/bg2.jpg" alt="background" height="600px" width="100%"></div>
@@ -28,7 +27,7 @@
                         <div class="Slide"><img src="<?= My_Files_Root ?>assets/images/slider-main/bg4.jpg" alt="background" height="600px" width="100%"></div>
                         <div class="Slide"><img src="<?= My_Files_Root ?>assets/images/slider-main/bg5.jpg" alt="background" height="600px" width="100%"></div>
                     </div>
-                    <img class="Slides_Next_Btn" id="Slides_Next_Btn" src="<?= My_Files_Root ?>assets/images/arrow-right-over.svg" alt="Next">
+                    <img class="Slides_Next_Btn" id="Slides_Next_Btn" src="<?= My_Files_Root ?>assets/images/arrow-right-out.svg" alt="Next">
                     <!-- If we need pagination 
                     <div class="swiper-pagination"></div>
 -->
@@ -51,41 +50,33 @@
             <div class="MostSearchedServices">
 
                 <div class="ServiceContainer">
-                    <h3>Service1</h3>
+                    <h3 class="ServiceName">Service1</h3>
                 </div>
 
                 <div class="ServiceContainer">
-                    <h3>Service1</h3>
+                    <h3 class="ServiceName">Service1</h3>
                 </div>
 
                 <div class="ServiceContainer">
-                    <h3>Service1</h3>
+                    <h3 class="ServiceName">Service1</h3>
                 </div>
                 <div class="ServiceContainer">
-                    <h3>Service1</h3>
+                    <h3 class="ServiceName">Service1</h3>
                 </div>
 
                 <div class="ServiceContainer">
-                    <h3>Service1</h3>
+                    <h3 class="ServiceName">Service1</h3>
                 </div>
 
                 <div class="ServiceContainer">
-                    <h3>Service1</h3>
+                    <h3 class="ServiceName">Service1</h3>
                 </div>
-
             </div>
-
-
         </section>
-
     </main>
     <?php include("../app/views/Footer.php"); ?>
 
     <script src="<?= My_Files_Root ?>assets/JS/script.js"></script>
-    <script src="<?= My_Files_Root ?>assets/JS/swiper-bundle.min.js"></script>
-
-
-
 </body>
 
 </html>
