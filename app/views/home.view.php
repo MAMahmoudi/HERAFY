ecebf59988ca99f45b12718d81d3ce34e4aecc78
Mahmoudi Mohamed Amine <mohamed.mahmoudi@ensia.edu.dz>
2023-08-26 19:33:48 +0100

    Successful_Projects

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="stylesheet" href="<?= My_Files_Root ?>assets/CSS/styles.css" />
     <link rel="stylesheet" href="<?= My_Files_Root ?>assets/CSS/Footer.css" />
+    <link rel="stylesheet" href="<?= My_Files_Root ?>assets/CSS/Successful_Projects.css" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,12 +25,11 @@
         ?>
 
         <section class="Services">
-
             <div class="SearchServices" id="SearchServices">
                 <input type="text" placeholder="Looking for a craftsman service...Start here!" class="SearchServicesTxtField" id="SearchServicesTxtField">
                 <ul id="AutoComplete_Results" class="AutoComplete_Results"></ul>
             </div>
-
+            <h1>الخدمات الأكثر طلبا</h1>
             <div class="MostSearchedServices">
 
                 <div class="ServiceContainer">
@@ -53,6 +53,49 @@
 
                 <div class="ServiceContainer">
                     <h3 class="ServiceName">Service1</h3>
+                </div>
+            </div>
+        </section>
+
+        <section class="Succuessful_Projects">
+            <h1>مشاريع ناحجة</h1>
+            <div class="Succuessful_Projects_Item">
+                <div class="Succuessful_Projects_Image">
+                    <img src="<?= My_Files_Root ?>assets/IMAGES/projects/project1.jpg" alt="image1" />
+                    <img src="<?= My_Files_Root ?>assets/IMAGES/projects/project2.jpg" alt="image2" />
+                </div>
+                <div className="Succuessful_Projects_Text">
+                    <h2>ddddddddddddddddddd</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto vel perspiciatis deleniti hic explicabo quis eius quo fugiat blanditiis odit quod, veritatis nesciunt maiores consectetur sint reiciendis nihil fuga. Pariatur?
+
+                    </p>
+                </div>
+            </div>
+            <div class="Succuessful_Projects_Item_Reverse">
+                <div class="Succuessful_Projects_Image">
+                    <img src="<?= My_Files_Root ?>assets/IMAGES/projects/project3.jpg" alt="image1" />
+                    <img src="<?= My_Files_Root ?>assets/IMAGES/projects/project4.jpg" alt="image2" />
+                </div>
+                <div className="Succuessful_Projects_Text">
+                    <h2>ddddddddddddddddddd</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto vel perspiciatis deleniti hic explicabo quis eius quo fugiat blanditiis odit quod, veritatis nesciunt maiores consectetur sint reiciendis nihil fuga. Pariatur?
+
+                    </p>
+                </div>
+            </div>
+            <div class="Succuessful_Projects_Item">
+                <div class="Succuessful_Projects_Image">
+                    <img src="<?= My_Files_Root ?>assets/IMAGES/projects/project5.jpg" alt="image1" />
+                    <img src="<?= My_Files_Root ?>assets/IMAGES/projects/project6.jpg" alt="image2" />
+                </div>
+                <div className="Succuessful_Projects_Text">
+                    <h2>ddddddddddddddddddd</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto vel perspiciatis deleniti hic explicabo quis eius quo fugiat blanditiis odit quod, veritatis nesciunt maiores consectetur sint reiciendis nihil fuga. Pariatur?
+
+                    </p>
                 </div>
             </div>
         </section>
